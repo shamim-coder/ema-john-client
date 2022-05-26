a52c186d96aba51b2a225b78c7a12e7dfc4fb30d
Shamim Reza <wpshamim@gmail.com>
2022-05-26 23:40:25 +0600

    add home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
+import homeImg from "../../Images/home-img.jpg";
+import "./Home.css";
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home</h1>
+        <div className="container">
+            <div className="row align-items-center home">
+                <div className="col-lg-8">
+                    <p className="mb-5 text-yellow">
+                        <small className="offer-text">Sale up to 70% off</small>
+                    </p>
+                    <div className="mb-5">
+                        <h1 className="fw-bolder home-title">New Collection For Fall</h1>
+                        <p className="home-text">Discover all the new arrivals of ready-to-wear collection.</p>
+                    </div>
+                    <button className="btn bg-yellow">SHOP NOW</button>
+                </div>
+                <div className="col-lg-4">
+                    <img className="home-img" src={homeImg} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
